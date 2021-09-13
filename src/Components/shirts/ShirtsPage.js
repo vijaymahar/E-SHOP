052d49645e388673be2aebcb26f2392f00e821ce
vijaymahar <vijaykumaranchupogu123@gmail.com>
2021-09-14 03:16:39 +0530

    shirts page filters functionality added

--- src/Components/shirts/ShirtsPage.js
+++ src/Components/shirts/ShirtsPage.js
@@ -1,11 +1,76 @@
-import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import React, { useState } from "react";
+import { Container, Row, Col, Form } from "react-bootstrap";
+import { TiDeleteOutline } from "react-icons/ti";
 // import ShirtsBanner from "../../assets/images/banners/shirtsBanner/men&women_banner.png";
 import banner from "../../assets/images/banners-main/shirtsBanner1.jpg";
 import Banner1 from "../../assets/images/banners/shirtsBanner/banner5.png";
 import ShowCards from "../common/ShowCards";
 import { shirtsData } from "../../data/ShowcadsData";
+import {
+  Brands,
+  SortBy,
+  Categories,
+  Price,
+  Color,
+  Discount,
+} from "../../data/FilterData";
 const ShirtsPage = () => {
+  const [value, setValue] = useState(10);
+  const [condition, setCondition] = useState(true);
+  const [loadbtn, setLoadbtn] = useState("+ show more");
+  // color filter
+  const [num, setNum] = useState(10);
+  const [logic, setLogic] = useState(true);
+  const [showBtn, setShowbtn] = useState("+ show more");
+  // color filter
+
+  // FILTER ADDING //
+  const [filterName, setFilter] = useState([]);
+  // FILTER ADDING //
+  const showMore = () => {
+    if (!condition) {
+      setValue(10);
+      setLoadbtn("+ show more");
+      setCondition(true);
+    } else {
+      setValue(Infinity);
+      setLoadbtn("- show less");
+      setCondition(false);
+    }
+  };
+  const viewMore = () => {
+    if (!logic) {
+      setNum(10);
+      setShowbtn("+ show more");
+      setLogic(true);
+    } else {
+      setNum(Infinity);
+      setShowbtn("- show less");
+      setLogic(false);
+    }
+  };
+
+  // ADD FILTER HANDLER //
+  const addFilter = (name) => {
+    setFilter([
+      ...filterName,
+      {
+        id: new Date().getTime(),
+        addedFilter: name,
+      },
+    ]);
+    console.log(filterName);
+  };
+  // ADD FILTER HANDLER //
+
+  // REMOVE FILTER //
+  const removeFilter = (id) => {
+    const newFilterList = filterName.filter((cur) => {
+      return cur.id !== id;
+    });
+    setFilter(newFilterList);
+  };
+  // REMOVE FILTER //
   return (
     <>
       <Container fluid className="g-0">
@@ -19,6 +84,166 @@ const ShirtsPage = () => {
         </Row>
         <Row>
           <img src={Banner1} alt="banner" className="w-100 h-auto my-5" />
+        </Row>
+        <Row className="d-flex g-0">
+          <Col md={2} className="px-2">
+            <span className="lead fw-bold text-center ms-4">Filters</span>
+            <Row>
+              {/*side part*/}
+              <Col md={12} lg={12} className="border my-2">
+                {/*brands*/}
+                <p className="brand_types">shop by brands</p>
+                {Brands.slice(0, value).map((cur) => {
+                  return (
+                    <div
+                      className="col-12"
+                      key={cur.id}
+                      onClick={() => addFilter(cur.name)}
+                    >
+                      <div
+                        className="form-check text-dark ms-2 my-2"
+                        id="brandList"
+                      >
+                        <label
+                          htmlFor={`brand${cur.id}`}
+                          className="form-check-label ms-1"
+                        >
+                          <input
+                            type="checkbox"
+                            className="form-check-input"
+                            id={`brand${cur.id}`}
+                          />
+                          {cur.name}
+                        </label>
+                      </div>
+                    </div>
+                  );
+                })}
+                <button id="loadMore_btn" onClick={showMore}>
+                  {loadbtn}
+                </button>
+              </Col>
+              {/*brands*/}
+              {/*categories*/}
+              <Col md={12} lg={12} className="border my-2">
+                <p className="brand_types">shop by Category</p>
+                {Categories.map((cur) => {
+                  return (
+                    <Col md={12} lg={12} key={cur.id}>
+                      <Form.Check type="checkbox">
+                        <Form.Check.Label id="catrgory_label">
+                          <Form.Check.Input
+                            type="checkbox"
+                            id="category_check"
+                          />
+                          {cur.type}
+                        </Form.Check.Label>
+                      </Form.Check>
+                    </Col>
+                  );
+                })}
+              </Col>
+              {/*categories*/}
+              {/* price */}
+              <Col md={12} lg={12} className="border my-2">
+                <p className="brand_types">shop by Price</p>
+                {Price.map((cur) => {
+                  return (
+                    <Col md={12} lg={12} key={cur.id}>
+                      <Form.Check type="checkbox">
+                        <Form.Check.Label id="catrgory_label">
+                          <Form.Check.Input
+                            type="checkbox"
+                            id="category_check"
+                          />
+                          {cur.name}
+                        </Form.Check.Label>
+                      </Form.Check>
+                    </Col>
+                  );
+                })}
+              </Col>
+              {/* price */}
+              {/* color */}
+              <Col md={12} lg={12} className="border">
+                <p className="brand_types">shop by Color</p>
+                {Color.slice(0, num).map((cur) => {
+                  const { code } = cur;
+                  return (
+                    <Col md={12} lg={12} key={cur.id}>
+                      <Form.Check type="checkbox">
+                        <Form.Check.Label id="catrgory_label">
+                          <Form.Check.Input
+                            type="checkbox"
+                            id="category_check"
+                            style={{ background: code }}
+                          />
+                          {cur.color}
+                        </Form.Check.Label>
+                      </Form.Check>
+                    </Col>
+                  );
+                })}
+                <button id="loadMore_btn" onClick={viewMore}>
+                  {showBtn}
+                </button>
+              </Col>
+              {/* color */}
+              {/* discount */}
+              <Col md={12} lg={12} className="border">
+                <p className="brand_types">shop by Dscount</p>
+                {Discount.map((cur) => {
+                  return (
+                    <Col md={12} lg={12} key={cur.id}>
+                      <Form.Check type="checkbox">
+                        <Form.Check.Label id="catrgory_label">
+                          <Form.Check.Input
+                            type="checkbox"
+                            id="category_check"
+                          />
+                          {cur.range}
+                        </Form.Check.Label>
+                      </Form.Check>
+                    </Col>
+                  );
+                })}
+              </Col>
+              {/* discount */}
+            </Row>
+            {/*side part*/}
+          </Col>
+          {/* ========================================================= */}
+          {/*main part*/}
+          <Col md={10} id="select_capsul">
+            <Col md={8} lg={8}>
+              {filterName.length !== 0
+                ? filterName.map((cur) => {
+                    return (
+                      <button id="filter_button" key={cur.id}>
+                        {cur.addedFilter}
+                        <TiDeleteOutline
+                          onClick={() => removeFilter(cur.id)}
+                          id="filterbtn_delete"
+                        />
+                      </button>
+                    );
+                  })
+                : null}
+            </Col>
+            <Col md={2} lg={2} className="ms-auto">
+              <select id="sort_by">
+                <option value="1">sort by</option>
+                {SortBy.map((cur) => {
+                  return (
+                    <option value={cur.name} key={cur.id}>
+                      {cur.name}
+                    </option>
+                  );
+                })}
+              </select>
+            </Col>
+          </Col>
+          {/*main part*/}
         </Row>
       </Container>
     </>
---
+++ src/data/FilterData.js
@@ -0,0 +1,325 @@
+export const SortBy = [
+  {
+    id: 1,
+    name: "watts's new",
+  },
+  {
+    id: 2,
+    name: "popularity",
+  },
+  {
+    id: 3,
+    name: "better discount",
+  },
+  {
+    id: 4,
+    name: "price high to low",
+  },
+  {
+    id: 5,
+    name: "price low to high",
+  },
+  {
+    id: 6,
+    name: "customer rating",
+  },
+];
+
+export const Brands = [
+  {
+    id: 1,
+    name: "adidas",
+  },
+  {
+    id: 2,
+    name: "allen solly",
+  },
+  {
+    id: 3,
+    name: "bullmer",
+  },
+  {
+    id: 4,
+    name: "celio",
+  },
+  {
+    id: 5,
+    name: "Calvin klein",
+  },
+  {
+    id: 6,
+    name: "dennison",
+  },
+  {
+    id: 7,
+    name: "flying machine",
+  },
+  {
+    id: 8,
+    name: "gap",
+  },
+  {
+    id: 9,
+    name: "H & m",
+  },
+  {
+    id: 10,
+    name: "Invictus",
+  },
+  {
+    id: 11,
+    name: "jack & jones",
+  },
+  {
+    id: 12,
+    name: "kook n keech",
+  },
+  {
+    id: 13,
+    name: "louis phillippe",
+  },
+  {
+    id: 14,
+    name: "Levis",
+  },
+  {
+    id: 15,
+    name: "mast & harbour",
+  },
+  {
+    id: 16,
+    name: "roadster",
+  },
+  {
+    id: 17,
+    name: "red tape",
+  },
+  {
+    id: 18,
+    name: "tommy hilfiger",
+  },
+  {
+    id: 19,
+    name: "van hussen",
+  },
+  {
+    id: 20,
+    name: "wrogn",
+  },
+];
+
+export const Categories = [
+  {
+    id: 1,
+    type: "mens",
+  },
+  {
+    id: 2,
+    type: "womens",
+  },
+  {
+    id: 3,
+    type: "brands",
+  },
+  {
+    id: 4,
+    type: "budget",
+  },
+  {
+    id: 5,
+    type: "comfirt",
+  },
+];
+
+export const Offers = [
+  {
+    id: 1,
+    name: "shop more save more",
+  },
+  {
+    id: 2,
+    name: "special price",
+  },
+  {
+    id: 3,
+    name: "black friday",
+  },
+  {
+    id: 4,
+    name: "no cost emi",
+  },
+  {
+    id: 5,
+    name: "by 1 get 1",
+  },
+  {
+    id: 6,
+    name: "flat 40% off",
+  },
+];
+
+export const Price = [
+  {
+    id: 1,
+    name: "Rs 249 to Rs 499",
+  },
+  {
+    id: 2,
+    name: "Rs 599 to Rs 999",
+  },
+  {
+    id: 3,
+    name: "Rs 1099 to Rs 1499",
+  },
+  {
+    id: 4,
+    name: "Rs 1599 to Rs 1999",
+  },
+  {
+    id: 5,
+    name: "Rs 2199 to Rs 2499",
+  },
+  {
+    id: 6,
+    name: "Rs 2599 to Rs 2999",
+  },
+  {
+    id: 7,
+    name: "Rs 3199 to Rs 3499",
+  },
+  {
+    id: 8,
+    name: "Rs 3599 to Rs 3999",
+  },
+  {
+    id: 9,
+    name: "Rs 4001 to Rs 6999",
+  },
+  {
+    id: 10,
+    name: "Rs 7001 to Rs 9999",
+  },
+];
+
+export const Discount = [
+  {
+    id: 1,
+    range: "10% and above",
+  },
+  {
+    id: 2,
+    range: "20% and above",
+  },
+  {
+    id: 3,
+    range: "30% and above",
+  },
+  {
+    id: 4,
+    range: "40% and above",
+  },
+  {
+    id: 5,
+    range: "50% and above",
+  },
+  {
+    id: 6,
+    range: "60% and above",
+  },
+  {
+    id: 7,
+    range: "70% and above",
+  },
+  {
+    id: 8,
+    range: "80% and above",
+  },
+];
+
+export const Color = [
+  {
+    id: 1,
+    color: "blue",
+    code: "#002fff",
+  },
+  {
+    id: 2,
+    color: "white",
+    code: "#ffffff",
+  },
+  {
+    id: 3,
+    color: "navy blue",
+    code: "#000080",
+  },
+  {
+    id: 4,
+    color: "black",
+    code: "#000000",
+  },
+  {
+    id: 5,
+    color: "green",
+    code: "#048b04",
+  },
+  {
+    id: 6,
+    color: "red",
+    code: "#ff0000",
+  },
+  {
+    id: 7,
+    color: "pink",
+    code: "#ffc1cb",
+  },
+  {
+    id: 8,
+    color: "beige",
+    code: "#f5f5dc",
+  },
+  {
+    id: 9,
+    color: "brown",
+    name: "#a52a2a",
+  },
+  {
+    id: 10,
+    color: "yellow",
+    code: "#ffff00",
+  },
+  {
+    id: 11,
+    color: "gold",
+    code: "#ffd900",
+  },
+  {
+    id: 12,
+    color: "tan",
+    code: "#d2b48c",
+  },
+  {
+    id: 13,
+    color: "lavender",
+    code: "#e6e6fa",
+  },
+  {
+    id: 14,
+    color: "multi color",
+    code: "conic-gradient(#ff0000 0deg, #ffa702 90deg, #ffff00 180deg, #048b04 270deg, #002fff 360deg)",
+  },
+  {
+    id: 15,
+    color: "steel",
+    code: "#a4a5a5",
+  },
+  {
+    id: 16,
+    color: "orange",
+    code: "#ffa702",
+  },
+  {
+    id: 17,
+    color: "off white",
+    code: "#9b9b9b",
+  },
+];
